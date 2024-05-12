a1da1b164a70e46a15821dddc877a39f62145d0e
Sagar Niroula <sagarniroula112@gmail.com>
2024-05-12 18:03:56 +0545

    last checkpoint: error handling

---
+++ server.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(con.connections);
+    console.log('DB connection success!');
+  });
+
+const port = 4000;
+app.listen(port, () => {
+  console.log('App is listening.....');
+});
