205b20ef98510711b9597e131443bc9d76a21ee9
Sagar Niroula <sagarniroula112@gmail.com>
2024-05-13 21:14:40 +0545

    Hashing user passwords done....

---
+++ models/userModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'Please tell us your name!'],
+    // trim: true,
+  },
+  email: {
+    type: String,
+    require: [true, 'Please tell us your email!'],
+    // trim: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email!'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password!'],
+    validate: {
+      // This only works on SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+// Runs between getting the data and saving it to the DB (for password encryption)
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified.
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
+// Required by users
+router.post('/signup', authController.signup);
+
+// Required by System Admin
 router
   .route('/')
   .get(userController.getAllUsers)
