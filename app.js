c171f5505aee28d19a16e99a5e2f510b52945907
Sagar Niroula <sagarniroula112@gmail.com>
2024-05-19 21:15:01 +0545

    cleaned app.js

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ app.use(morgan('dev'));
 app.use(express.json()); // middleware (for req.body)
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   console.log('Hello from the middleware!');
-//   next();
-// });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -31,9 +26,6 @@ app.use('/api/v1/users', userRouter);
 
 // Neither tourRouter nor userRouter was able to catch it
 app.all('*', (req, res, next) => {
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
